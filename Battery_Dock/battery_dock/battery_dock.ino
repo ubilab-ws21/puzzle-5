7c287008ede0f4521b6527dc7c710ea90367b725
Terry Tsang <archerindigo@gmail.com>
2022-01-11 17:24:14 +0100

    Fix WiFi error handling; Add card UID identification

--- Battery_Dock/battery_dock/battery_dock.ino
+++ Battery_Dock/battery_dock/battery_dock.ino
@@ -10,6 +10,9 @@
 #define DEBUG 1
 #define DEBUG_OFFLINE 0
 
+// Puzzle setup
+const uint8_t BATTERY_UID[4] = {0xFE, 0x88, 0x50, 0x10};
+
 // Network configurations
 #define WLAN_SSID "ssid"
 #define WLAN_PASS "passphase"
@@ -25,9 +28,10 @@ Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 void setup() {
   Serial.begin(115200);
   Serial.println("\nSerial: up");
-  if(setupWiFi(WLAN_TIMEOUT))
+#if !DEBUG_OFFLINE
+  if(!setupWiFi(WLAN_TIMEOUT))
     ESP.restart();
-
+#endif
   setupNFC();
 }
 
@@ -37,13 +41,23 @@ void loop() {
   uint8_t uid[] = { 0, 0, 0, 0, 0, 0, 0 };  // Buffer to store the returned UID
   uint8_t uidLength;                        // Length of the UID (4 or 7 bytes depending on ISO14443A card type)
 
-  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength);
-  if (success) {
+  Serial.println("\n\nPlace a card to read!");
+  success = nfc.readPassiveTargetID(PN532_MIFARE_ISO14443A, uid, &uidLength, 0);  // timeout=0 -> blocking
+  if(success) {
     // Display some basic information about the card
     Serial.println("Found an ISO14443A card");
     Serial.print("  UID Length: ");Serial.print(uidLength, DEC);Serial.println(" bytes");
     Serial.print("  UID Value: ");
     nfc.PrintHex(uid, uidLength);
+
+    if(memcmp(BATTERY_UID, uid, 4) == 0) {
+      Serial.println("Detected the battery!");
+    }
+  }
+  else {
+    // Terry: Found some cards will make the read function fail forever. Restart is needed.
+    Serial.println("Failed to read a card! Try to reset.");
+    setupNFC();
   }
 
   delay(1000);
@@ -51,6 +65,9 @@ void loop() {
 
 void setupNFC() {
   nfc.begin();
+
+  // Try to get firmware version of the reader
+  // Not guarantee to success in initial run!
   uint32_t versiondata = nfc.getFirmwareVersion();
   Serial.print("NFC chip PN5"); Serial.println((versiondata>>24) & 0xFF, HEX); 
   Serial.print("Firmware ver. "); Serial.print((versiondata>>16) & 0xFF, DEC); 
@@ -73,6 +90,7 @@ bool setupWiFi(unsigned int timeout) {
   if(counter) {
     Serial.println("\nWiFi connected.\nIP address: ");
     Serial.println(WiFi.localIP());
+    return true;
   }
 
   Serial.println("Wifi not connecting...");
