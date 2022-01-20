d7693e79e575137bacf6fc196738b89a7d12ad91
Terry Tsang <archerindigo@gmail.com>
2022-01-20 12:16:55 +0100

    Add basic LCD support to battery dock's code

--- Battery_Dock/battery_dock/battery_dock.ino
+++ Battery_Dock/battery_dock/battery_dock.ino
@@ -6,12 +6,13 @@
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_PN532.h>
+#include <LiquidCrystal.h>
 
-#define DEBUG 1
+#define DEBUG 0
 #define DEBUG_OFFLINE 0
 
 // Puzzle setup
-const uint8_t BATTERY_UID[4] = {0xFE, 0x88, 0x50, 0x10};
+const uint8_t BATTERY_UID[4] = {0xB3, 0x5B, 0xF2, 0xBB};
 
 // Network configurations
 #define WLAN_SSID "ssid"
@@ -25,7 +26,70 @@ const uint8_t BATTERY_UID[4] = {0xFE, 0x88, 0x50, 0x10};
 #define PN532_MISO (19)
 Adafruit_PN532 nfc(PN532_SCK, PN532_MISO, PN532_MOSI, PN532_SS);
 
+// LCD
+// Note: GPIO12 on ESP32 NodeMCU may not work
+// The initializer of LiquidCrystal cotain begin(1, 16)
+// if it is called before setup(), ESP32 NodeMCU may crash
+const int LCD_rs = 14, LCD_rw = 13, LCD_en = 27, LCD_d4 = 26, LCD_d5 = 25, LCD_d6 = 33, LCD_d7 = 32;
+LiquidCrystal lcd(LCD_rs, LCD_rw, LCD_en, LCD_d4, LCD_d5, LCD_d6, LCD_d7);
+byte LCD_CHAR_BLOCK[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte LCD_CHAR_LEFT_EDGE[8] = {
+  B11111,
+  B10000,
+  B10000,
+  B10000,
+  B10000,
+  B10000,
+  B11111,
+};
+
+byte LCD_CHAR_MID_EDGE[8] = {
+  B11111,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+};
+
+byte LCD_CHAR_RIGHT_EDGE[8] = {
+  B11111,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B00001,
+  B11111,
+};
+
+enum {
+  LCD_BLOCK,
+  LCD_LEFT_EDGE,
+  LCD_MID_EDGE,
+  LCD_RIGHT_EDGE
+};
+
+// Game status variables
+int BatteryLv = 0;
+
 void setup() {
+  // LCD setup
+  lcd.begin(20, 4);
+  lcd.createChar(LCD_BLOCK, LCD_CHAR_BLOCK);
+  lcd.createChar(LCD_LEFT_EDGE, LCD_CHAR_LEFT_EDGE);
+  lcd.createChar(LCD_MID_EDGE, LCD_CHAR_MID_EDGE);
+  lcd.createChar(LCD_RIGHT_EDGE, LCD_CHAR_RIGHT_EDGE);
+  
   Serial.begin(115200);
   Serial.println("\nSerial: up");
 #if !DEBUG_OFFLINE
@@ -60,7 +124,7 @@ void loop() {
     setupNFC();
   }
 
-  delay(1000);
+  delay(500);
 }
 
 void setupNFC() {
@@ -95,4 +159,33 @@ bool setupWiFi(unsigned int timeout) {
 
   Serial.println("Wifi not connecting...");
   return false;
+}
+
+void printBatteryLv()
+{
+  int i = 0;
+  lcd.clear();
+  lcd.home();
+  lcd.print("Battery Level:");
+  lcd.setCursor(0, 1);
+  if (BatteryLv == 0) {
+    lcd.write(byte(LCD_LEFT_EDGE));
+  }
+  else {
+    for(i=0; i<BatteryLv/10; i++) {
+      lcd.write(byte(LCD_BLOCK));
+    }
+  }
+
+  for(; i<9 ; i++) {
+    lcd.write(byte(LCD_MID_EDGE));
+  }
+  if(BatteryLv < 100)
+    lcd.write(byte(LCD_RIGHT_EDGE));
+  else
+    lcd.write(byte(LCD_BLOCK));
+
+  lcd.print(" ");
+  lcd.print(BatteryLv);
+  lcd.print("%");
 }
